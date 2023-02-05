2e2a13d9f809fd502b1be1c6a0b2b4d7f8b6055c
Gareth Sears <gareth@migratehr.com>
2023-02-05 11:58:19 +0000

    Clean up Exercise1_1

--- src/main/scala/part2actors/Exercise1_1.scala
+++ src/main/scala/part2actors/Exercise1_1.scala
@@ -4,27 +4,33 @@ import akka.actor.ActorSystem
 import akka.actor.Actor
 import akka.actor.Props
 
-  class Counter extends Actor {
-    var count = 0;
+// Domain of the counter
+object Counter {
+  case object Increment
+  case object Decrement
+  case object Print
+}
 
-    def receive: Receive = {
-      case Increment => count += 1;
-      case Decrement => count -= 1;
-      case Print     => println(s"My count is: $count")
-    }
+class Counter extends Actor {
+  import Counter._
+  
+  var count = 0;
+
+  def receive: Receive = {
+    case Increment => count += 1;
+    case Decrement => count -= 1;
+    case Print     => println(s"My count is: $count")
   }
-
-  case class Increment();
-  case class Decrement();
-  case class Print();
+}
 
 object Exercise1_1 extends App {
+  import Counter._
+
   val system = ActorSystem("actorSystem");
   val counter = system.actorOf(Props[Counter](), "counter")
-  
-  counter ! Increment
-  counter ! Increment
-  counter ! Decrement
+
+  (1 to 5).foreach(_ => counter ! Increment)
+  (1 to 3).foreach(_ => counter ! Decrement)
   counter ! Print
 
   system.terminate();
