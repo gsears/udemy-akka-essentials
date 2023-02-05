b893a4e2f4921221fcfa136e160e699601e7a0ad
Gareth Sears <gareth@migratehr.com>
2023-02-05 12:18:06 +0000

    Clean up Exercise1_2

--- src/main/scala/part2actors/Exercise1_2.scala
+++ src/main/scala/part2actors/Exercise1_2.scala
@@ -7,73 +7,84 @@ import scala.util.Failure
 import akka.actor.Props
 import akka.actor.ActorRef
 
-case class Deposit(amount: Int)
-case class Withdraw(amount: Int)
-case class PrintStatement()
-class InsufficientFundsError extends Exception("Insufficient funds")
+object BankAccount {
+  case class Deposit(amount: Int)
+  case class Withdraw(amount: Int)
+  case class TransactionSuccess(message: String)
+  case class TransactionFailure(message: String)
+  case object PrintStatement
+}
 
 class BankAccount extends Actor {
+  import BankAccount._
+
   var balance = 0
 
   def receive: Receive = {
-    case Deposit(amount) if amount > 0 =>
-      balance += amount
-      println("changing balance")
-      context.sender() ! Success("Deposit success")
     case Deposit(amount) =>
-      context.sender() ! Failure(new Exception(s"Deposit must be positive, received $amount"))
-    case Withdraw(amount) if amount > 0 =>
-      val provisionalBalance = balance - amount
-      if (provisionalBalance < 0)
-        context.sender() ! Failure(new InsufficientFundsError)
-      else
-        balance = provisionalBalance;
-        context.sender () ! Success("Withdrawal success")
+      if (amount <= 0)
+        context.sender() ! TransactionFailure(
+          s"Deposit must be greater than 0. Received $amount"
+        )
+      else {
+        balance += amount
+        context.sender() ! TransactionSuccess(s"Deposited $amount")
+      }
     case Withdraw(amount) =>
-      Failure(new Exception(s"Withdrawal must be positive, received $amount"))
-    case PrintStatement => println(s"[${self.path}] Balance: ${balance}")
-    }
+      if (amount <= 0)
+        context.sender() ! TransactionFailure(
+          s"Deposit must be greater than 0. Received $amount"
+        )
+      else if (amount > balance)
+        context.sender() ! TransactionFailure(
+          "Insufficient funds"
+        )
+      else {
+        balance -= amount
+        context.sender() ! TransactionSuccess(s"Withdrew $amount")
+      }
+    case PrintStatement => println(balance)
+  }
 }
 
-case class AttemptDeposit(amount: Int, ref: ActorRef)
-case class AttemptWithdraw(amount: Int, ref: ActorRef)
+object AccountHolder {
+  case class AttemptDeposit(amount: Int, ref: ActorRef)
+  case class AttemptWithdraw(amount: Int, ref: ActorRef)
+}
 
 class AccountHolder extends Actor {
-    def receive: Receive = {
-        case AttemptDeposit(amount, ref) =>
-            println("Attempting deposit")
-            ref ! Deposit(amount)
-        case AttemptWithdraw(amount, ref) =>
-            println("Attempting withdrawal")
-            ref ! Withdraw(amount) 
-            
-        case Success(f) => 
-            println(s"[${self.path}] :D")
-            context.sender() ! PrintStatement
-        
-        case Failure(e: InsufficientFundsError) => 
-            println(s"[${self.path}] D: No monies!")
-            context.sender() ! PrintStatement
-        
-        case Failure(e) =>
-              println(s"[${self.path}] D:")
-              println(e.toString())
-    
-        }
-} 
+  import AccountHolder._
+  import BankAccount._
+  def receive: Receive = {
+    case AttemptDeposit(amount, ref) =>
+      ref ! Deposit(amount)
+
+    case AttemptWithdraw(amount, ref) =>
+      ref ! Withdraw(amount)
+
+    case TransactionSuccess(msg) =>
+      println(s"Machine says: $msg :D")
+
+    case TransactionFailure(msg) =>
+      println(s"Machine says: $msg D:")
+
+  }
+}
 
 object Exercise1_2 extends App {
-  val system = ActorSystem("system");
+  import AccountHolder._
+  import BankAccount._
 
+  val system = ActorSystem("system");
 
   val myAccount = system.actorOf(Props[BankAccount](), "myAccount")
   val me = system.actorOf(Props[AccountHolder](), "me")
 
   me ! AttemptDeposit(200, myAccount)
-    me ! AttemptDeposit(200, myAccount)
-    me ! AttemptDeposit(0, myAccount)
-    me ! AttemptWithdraw(500, myAccount)
-    me ! AttemptDeposit(100, myAccount)
-    me ! AttemptWithdraw(500, myAccount)
-//   system.terminate()
+  me ! AttemptDeposit(200, myAccount)
+  me ! AttemptDeposit(0, myAccount)
+  me ! AttemptWithdraw(500, myAccount)
+  me ! AttemptDeposit(100, myAccount)
+
+  myAccount ! PrintStatement
 }
